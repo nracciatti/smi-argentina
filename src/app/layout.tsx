8f2824e02a17d2d81b7b64626cfe32a60e7fe501
Nico Racciatti <nicoracciatti1@gmail.com>
2026-06-18 10:18:49 -0300

    Add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import "./globals.css";
 import { site } from "@/lib/site";
+import { GOOGLE_ADS_ID } from "@/lib/googleAds";
 
 export const metadata: Metadata = {
   metadataBase: new URL(`https://${site.domain}`),
@@ -52,9 +53,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <Script id="google-tag-manager" strategy="beforeInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T6GJQZZ7');
+        `}
+      </Script>
       <Script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=AW-18217037623"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`}
         strategy="afterInteractive"
       />
       <Script id="google-ads-tag" strategy="afterInteractive">
@@ -63,10 +73,20 @@ export default function RootLayout({
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'AW-18217037623');
+          gtag('config', '${GOOGLE_ADS_ID}');
         `}
       </Script>
-      <body>{children}</body>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T6GJQZZ7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
